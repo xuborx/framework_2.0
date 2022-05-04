be07a73e950aac0ca2c510ba5d199472a9e9a14f
Denys Korkh <denys.korkh@symmetrics.de>
2022-05-04 18:20:18 +0300

    Enabled display of errors depending on the DEBUG constant

--- vendor/xuborx/framework20/src/App.php
+++ vendor/xuborx/framework20/src/App.php
@@ -15,6 +15,11 @@ class App
      */
     public static function start(): void
     {
+        if (DEBUG) {
+            error_reporting(E_ALL);
+            ini_set('display_errors', '1');
+        }
+
         session_start();
 
         if (USE_REDBEAN) {
